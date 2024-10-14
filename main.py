25e0aea97feb952e4e5f1fc4452cadc7098b4f6e
lythanhdat21 <83631595+lythanhdat21@users.noreply.github.com>
2024-10-15 03:13:30 +0700

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit.components.v1 as components  # Nhập components module
+import streamlit.components.v1 as components
 import requests
 
 # Tải file HTML từ GitHub
@@ -10,4 +10,9 @@ if response.status_code == 200:
     html_content = response.text
     components.html(html_content, height=300, scrolling=True)  # Sử dụng components.html
 else:
-    st.error("Không thể tải file HTML từ GitHub.")
+    if response.status_code == 404:
+        st.error("Không tìm thấy file HTML trên GitHub.")
+    elif response.status_code == 403:
+        st.error("Truy cập bị từ chối. Kiểm tra quyền truy cập tới file HTML.")
+    else:
+        st.error(f"Có lỗi xảy ra: {response.status_code}.")
